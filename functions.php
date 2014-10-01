8e99a69d7cc85dde6c0584877b09a1b00117cb02
Jeremy Herve <jeremy@tagada.hu>
2014-10-01 10:10:50 +0000

    Site Logo: add support for Jetpack's Site Logo feature

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@
 function gustavo_setup() {
 	// Textdomain
 	load_child_theme_textdomain( 'gustavo', get_stylesheet_directory() . '/languages' );
+
+	// Site Logo
+	add_theme_support( 'site-logo', array( 'size' => 'gustavo-logo' ) );
+
+	add_image_size( 'gustavo-logo', 512, 512 );
 }
 add_action( 'after_setup_theme', 'gustavo_setup' );
 
